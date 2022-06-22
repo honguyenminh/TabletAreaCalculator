05aa8b102df5ff9cd147b390bf52abb65485a8f6
Hồ Nguyên Minh <honguyenminh@outlook.com>
2022-06-23 00:17:41 +0700

    Add existing code

---
+++ TabletAreaCalculator/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TabletAreaCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private MainWindowData _data;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _data = new MainWindowData();
+            DataContext = _data;
+            AutoSetResolutionButton_OnClick(null, null);
+        }
+
+        private void AutoSetResolutionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _data.ScreenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            _data.ScreenHeight = (int)SystemParameters.PrimaryScreenHeight;
+        }
+    }
+}
---
+++ TabletAreaCalculator/MainWindowData.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TabletAreaCalculator
+{
+    public class MainWindowData : INotifyPropertyChanged
+    {
+        private int _screenHeight;
+        public int ScreenHeight
+        {
+            get => _screenHeight;
+            set
+            {
+                _screenHeight = Math.Abs(value);
+                OnPropertyChanged();
+            }
+        }
+        
+        private int _screenWidth;
+        public int ScreenWidth
+        {
+            get => _screenWidth;
+            set
+            {
+                _screenWidth = Math.Abs(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private double _scaleFactor;
+        public double ScaleFactor
+        {
+            get => _scaleFactor;
+            set
+            {
+                if (value > 100) value = 100;
+                else if (value < 0) value = 0;
+                value = Math.Round(value, 2);
+                _scaleFactor = value;
+                OnPropertyChanged();
+            }
+        }
+        // TODO: delete these default values after done using them
+        private int _tabletWidth = 4000;
+        public int TabletWidth
+        {
+            get => _tabletWidth;
+            set
+            {
+                _tabletWidth = Math.Abs(value);
+                OnPropertyChanged();
+            }
+        }
+        
+        private int _tabletHeight = 3000;
+        public int TabletHeight
+        {
+            get => _tabletHeight;
+            set
+            {
+                _tabletHeight = Math.Abs(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private int _finalHeight;
+        public int FinalHeight
+        {
+            get => _finalHeight;
+            protected set
+            {
+                _finalHeight = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private int _finalWidth;
+        public int FinalWidth
+        {
+            get => _finalWidth;
+            protected set
+            {
+                _finalWidth = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private void CalculateFinalResolution()
+        {
+            double tabletRatio = (double)TabletWidth / TabletHeight;
+            double screenRatio = (double)ScreenWidth / ScreenHeight;
+            bool keepWidth = tabletRatio < screenRatio;
+            if (keepWidth)
+            {
+                FinalHeight = (int)((float)TabletWidth / ScreenWidth * ScreenHeight * ScaleFactor / 100);
+                FinalWidth = (int)(TabletWidth * ScaleFactor / 100);
+            }
+            else
+            {
+                FinalWidth = (int)((float)TabletHeight / ScreenHeight * ScreenWidth * ScaleFactor / 100);
+                FinalHeight = (int)(TabletHeight * ScaleFactor / 100);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName != nameof(FinalHeight) && propertyName != nameof(FinalWidth))
+                CalculateFinalResolution();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
